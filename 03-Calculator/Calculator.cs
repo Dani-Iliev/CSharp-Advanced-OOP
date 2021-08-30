9a37c7ad38dc3e388fbeb39a41e6c70cefb3d15f
Dani-Iliev <75176422+Dani-Iliev@users.noreply.github.com>
2021-08-30 21:01:56 +0300

    Add Calculator

---
+++ 03-Calculator/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            var expression = Console
+                .ReadLine()
+                .Split()
+                .Reverse()
+                .ToList();
+
+            var stack = new Stack<string>(expression);
+
+            while (stack.Count > 1)
+            {
+                int firstNumber = int.Parse(stack.Pop());
+                string symbol = stack.Pop();
+                int secondNumber = int.Parse(stack.Pop());
+
+                if (symbol == "+")
+                {
+                    stack.Push((firstNumber + secondNumber).ToString());
+                }
+                else if (symbol == "-")
+                {
+                    stack.Push((firstNumber - secondNumber).ToString());
+                }
+                else if (symbol == "*")
+                {
+                    stack.Push((firstNumber * secondNumber).ToString());
+                }
+                else if (symbol == "/")
+                {
+                    stack.Push((firstNumber / secondNumber).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    
+    }
+}
