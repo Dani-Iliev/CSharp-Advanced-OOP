b43139fd3958bd32e1aeffd74a54735169d8d0b8
Dani-Iliev <75176422+Dani-Iliev@users.noreply.github.com>
2021-08-30 21:06:21 +0300

    Add Matching Brackets

---
+++ 04-MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_MatchingBrackets
+{
+    class MatchingBrackets
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int currentIndex = i;
+                    int openingBracket = stack.Pop();
+
+                    string subString = expression.Substring(openingBracket, currentIndex - openingBracket + 1);
+                    Console.WriteLine(subString);
+                }
+            }
+        }
+    }
+}
